b69bd6fbd2a9aaee10f06b022b22aada0ad7855f
David <dngames2006@gmail.com>
2019-09-28 13:32:41 -0500

    Made program function

--- search.py
+++ search.py
@@ -1,11 +1,18 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="Find text in some text")
-parser.add_argument('inputText', type=str, help="A string to look for")
+parser.add_argument('inputText1', type=str, help="A string to look for")
+parser.add_argument('inputText2', type=str, help="A string to compare")
 
 args = parser.parse_args()
-inputString = args.inputText
-outputString = ""
+inputString1 = args.inputText1
+inputString2 = args.inputText2
 
 def search(a, b):
-    pass
+    if a in b:
+        return b
+    else:
+        return False
+
+outputString = search(inputString1, inputString2)
+print(outputString)
